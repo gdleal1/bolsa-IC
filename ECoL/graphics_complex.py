ab82eef78e88a56aaada0cefb3f3f60a5003c7b7
Gabriel <gleal0607@gmail.com>
2024-11-29 10:04:16 -0300

    little changes

--- ECoL/graphics_complex.py
+++ ECoL/graphics_complex.py
@@ -2,28 +2,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-NAME_DS1 = 'female'
-NAME_DS2 = 'male'
-TITLE = f'Difference of the complexity measures between {NAME_DS1} and {NAME_DS2} datasets'
-
+NAME_DATASET = 'female'
+TITLE = f'Complexity measures of the {NAME_DATASET} dataset'
 
 def create():
     
-    file_path = f'outputs_diff/{NAME_DS1}-{NAME_DS2}-diff.csv'  
+    file_path = f'outputs_complex/output-{NAME_DATASET}.csv'  
     data = pd.read_csv(file_path)
-
-    # Different colors for positive and negative values
-    colors = ['cornflowerblue' if value > 0 else 'lightcoral' for value in data['Value']]
 
     # Bar graphic
     plt.figure(figsize=(10, 6))
-    plt.bar(data['Complexity Measure'], data['Value'], color=colors)
+    plt.bar(data['Complexity Measure'], data['Value'], color='cornflowerblue')
 
     # Line graphic
     plt.plot(data['Complexity Measure'], data['Value'], color='darkorange', label='Linha')
-
-    # Set y-axis limits
-    plt.ylim(-0.15, 0.3)
 
     plt.title(TITLE)
     plt.xlabel('Complexity Measure')
@@ -31,7 +23,7 @@ def create():
     plt.xticks(rotation=45)  
     plt.tight_layout()  
 
-    graphic_path = f"graphics/{NAME_DS1}-{NAME_DS2}-diff_graphic.png"
+    graphic_path = f"graphics/{NAME_DATASET}_graphic.png"
     plt.savefig(graphic_path, dpi=300)
 
 if __name__ == "__main__":
